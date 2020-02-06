f837b660f70a06d3a3c0e6d2245240bfa20e05d0
Sage-Kearney <56275819+Sage-Kearney@users.noreply.github.com>
2020-02-06 15:38:59 -0500

    Show view rendering

--- controllers/books.js
+++ controllers/books.js
@@ -7,9 +7,15 @@ router.get('/', (req, res) => {
     Book.find({})
     .then(books => {
         res.render('index', { books });
-    })
-})
+    });
+});
 
+router.get('/:title', (req, res) => {
+    Book.find({ title: req.params.title})
+    .then(book => {
+        res.render('show', book[0] );
+    });
+});
 
 
 module.exports = router;
