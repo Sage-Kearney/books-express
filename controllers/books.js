51de1f9efcf717a9a87b18a97b9a79cb8b65a052
aafmisra <aafmisra@gmail.com>
2020-02-06 16:46:51 -0500

    edit view rendering

--- controllers/books.js
+++ controllers/books.js
@@ -4,18 +4,34 @@ const Book = require('../db/schema');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    Book.find({})
-    .then(books => {
-        res.render('index', { books });
-    });
+  Book.find({}).then(books => {
+    res.render('index', { books });
+  });
+});
+
+router.get('/:title/edit', (req, res) => {
+  Book.findOne({ title: req.params.title }).then(book => {
+    res.render('edit', book);
+  });
+});
+
+router.put('/:title', (req, res) => {
+  const read = req.body.read === 'on' ? true : false;
+  const book = {
+    review: req.body.review,
+    read
+  };
+  Book.findOneAndUpdate({ title: req.params.title }, book, { new: true }).then(
+    book => {
+      res.redirect('/books');
+    }
+  );
 });
 
 router.get('/:title', (req, res) => {
-    Book.find({ title: req.params.title})
-    .then(book => {
-        res.render('show', book[0] );
-    });
+  Book.find({ title: req.params.title }).then(book => {
+    res.render('show', book[0]);
+  });
 });
-
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const booksController = require('./controllers/books');
 const express = require('express');
+const methodOverride = require('method-override');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'hbs');
+
+app.use(methodOverride('_method'));
 
 app.use('/books', booksController);
 
 app.listen(4000, () => {
-    console.log('app listening on port 4000');
+  console.log('app listening on port 4000');
 });
